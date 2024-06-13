19b95961f7c5f38c08d3aebf0f25f56cd3841f33
PrammP <92103872+PrammP@users.noreply.github.com>
2024-06-13 14:37:35 +0200

    deadline

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+const deadlineDateInput = document.getElementById("deadline-date");
 
 function addTask() {
   if (inputBox.value === "") {
@@ -9,9 +10,18 @@ function addTask() {
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
 
-    let Date = document.createElement("p");
-    Date.innerHTML = getTodayDate() + getTodayHour();
-    li.appendChild(Date);
+    let dateElement = document.createElement("p");
+    dateElement.innerHTML = getTodayDate() + getTodayHour();
+    li.appendChild(dateElement);
+
+    if (deadlineDateInput.value) {
+      let remainingDaysElement = document.createElement("p");
+      remainingDaysElement.innerHTML =
+        "Temps restant: " +
+        calculateRemainingDays(deadlineDateInput.value) +
+        " jours";
+      li.appendChild(remainingDaysElement);
+    }
 
     let spanDelete = document.createElement("span");
     spanDelete.innerHTML = "\u00d7";
@@ -24,6 +34,7 @@ function addTask() {
     li.appendChild(spanEdit);
   }
   inputBox.value = "";
+  deadlineDateInput.value = "";
   saveData();
 }
 
@@ -62,6 +73,20 @@ function editTask(li) {
 
   input.addEventListener("blur", function () {
     li.innerHTML = input.value;
+
+    let dateElement = document.createElement("p");
+    dateElement.innerHTML = getTodayDate() + getTodayHour();
+    li.appendChild(dateElement);
+
+    if (deadlineDateInput.value) {
+      let remainingDaysElement = document.createElement("p");
+      remainingDaysElement.innerHTML =
+        "Temps restant: " +
+        calculateRemainingDays(deadlineDateInput.value) +
+        " jours";
+      li.appendChild(remainingDaysElement);
+    }
+
     let spanDelete = document.createElement("span");
     spanDelete.innerHTML = "\u00d7";
     spanDelete.classList.add("delete");
@@ -72,13 +97,10 @@ function editTask(li) {
     spanEdit.classList.add("edit");
     li.appendChild(spanEdit);
 
-    let Date = document.createElement("p");
-    Date.innerHTML = getTodayDate() + getTodayHour();
-    li.appendChild(Date);
-
     saveData();
   });
 }
+
 function saveData() {
   localStorage.setItem("data", listContainer.innerHTML);
 }
@@ -99,9 +121,18 @@ function getTodayDate() {
 function getTodayHour() {
   let today = new Date();
   let hours = String(today.getHours());
-  let minutes = String(today.getMinutes());
+  let minutes = String(today.getMinutes()).padStart(2, "0");
 
   return ` ${hours}:${minutes}`;
+}
+
+function calculateRemainingDays(deadline) {
+  let today = new Date();
+  let deadlineDate = new Date(deadline);
+  let timeDiff = deadlineDate - today;
+  let daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+
+  return daysDiff >= 0 ? daysDiff : 0;
 }
 
 showTask();
